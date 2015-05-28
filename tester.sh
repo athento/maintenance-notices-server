c0c43b05c3b7c9ecc150dcb8b87a656f14f9c459
Antonio García <tarazaky@alu.uma.es>
2015-05-28 11:12:50 +0200

    Update tester.sh

--- tester.sh
+++ tester.sh
@@ -1,6 +1,6 @@
-curl admin:admin@localhost:8000/index/create/ \
---data "domain=localhost:8080\$Default Domain 2" \
---data "message=mensajito :D" \
---data "user=vm" \
+curl http://msg.server.com/index/create/ \
+--data "domain=your.server.com\$Default Domain 2" \
+--data "message=<p>Server is going down for maintenance at 10:00 UTC. </p>" \
+--data "user=manager" \
 --data "start_date=2015-05-24 00:00" \
 --data "end_date=2015-06-5 00:00"
